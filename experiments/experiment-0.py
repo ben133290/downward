9cee9542ab43cb5c420237011414f127353b4535
ben133290 <benheuser2@gmail.com>
2026-04-28 10:15:51 +0200

    update experiments

--- experiments/experiment-0.py
+++ experiments/experiment-0.py
@@ -2,11 +2,9 @@
 
 import os
 import custom_parser
-from lab.environments import LocalEnvironment, BaselSlurmEnvironment
-from lab.experiment import Experiment
-from downward.experiment import FastDownwardExperiment
+from lab.environments import BaselSlurmEnvironment
 import common_setup
-from common_setup import IssueConfig, IssueExperiment
+from common_setup import OptionsConfig, TranslatorExperiment
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.environ["DOWNWARD_REPO"]
@@ -17,9 +15,9 @@ CONFIG_NICKS = [
     ("astar-blind", ["--search", "astar(blind())"]),
 ]
 CONFIGS = [
-    IssueConfig(
-        config_nick,
-        config,
+    OptionsConfig(
+        nick=config_nick,
+        component_options=config,
         build_options=[build],
         driver_options=['--search-time-limit', '5m', "--build", build])
     for build in BUILDS
@@ -41,7 +39,7 @@ ENVIRONMENT = BaselSlurmEnvironment(
 print(BENCHMARKS_DIR)
 print(REPO_DIR)
 
-exp = FastDownwardExperiment(path=REPO_DIR, environment=ENVIRONMENT)
+exp = TranslatorExperiment(repo_path=REPO_DIR, revisions=REVISIONS, configs=CONFIGS, path=None)
 
 exp.add_suite(BENCHMARKS_DIR, SUITE)
 
@@ -55,8 +53,6 @@ exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_step('parse', exp.parse)
 exp.add_fetcher(name='fetch')
-
-exp.add_algorithm("base", REPO_DIR, REVISIONS, ["--translate-options", "--eliminate-disjunctions=none", "--search-options"], build_options=None, driver_options=None)
 
 #exp.add_absolute_report_step(attributes=exp.DEFAULT_TABLE_ATTRIBUTES + ["search_start_time"])
 # exp.add_comparison_table_step(attributes=exp.DEFAULT_TABLE_ATTRIBUTES + ["search_start_time"])
