c44e3b4d309fc12703e63dc4718f4b8c34d18226
ben133290 <benheuser2@gmail.com>
2026-08-17 14:12:59 +0200

    add stats for strongly connected components

--- src/translate/axiom_rules.py
+++ src/translate/axiom_rules.py
@@ -106,6 +106,8 @@ def get_strongly_connected_components(dependencies):
 
     index_groups = sccs.get_sccs_adjacency_list(adjacency_list)
     groups = [[sorted_vars[i] for i in g] for g in index_groups]
+    print(f"Number of SCCs: {len(groups)}")
+    print(f"Sizes of SCCs: {[len(group) for group in groups]}")
     return groups
 
 # Expects a list of axioms *with the same head* and returns a subset consisting
